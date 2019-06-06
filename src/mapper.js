1d6e6bdf54ca3e30226e78e380d3a56c3f925473
VL K <vladimir.krichtak@sprinx.com>
2019-06-06 19:43:10 +0200

    implementing mapper

--- src/mapper.js
+++ src/mapper.js
@@ -6,7 +6,12 @@
  * @return {Function}     Mapping function
  */
 const stopsCalculationMapper = (distance, parsers, stopsCalculation) => (data) => data.map((p) => {
-
+  const stops = stopsCalculation(
+    distance,
+    parsers.parseConsumables(p.consumables),
+    parsers.parseMGLT(p.MGLT),
+  );
+  return `${p.name}: ${stops}`;
 });
 
 export default stopsCalculationMapper;
