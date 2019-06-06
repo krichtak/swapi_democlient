6da59bd1b4a8c8977eba987851efc00c4bc96501
VL K <vladimir.krichtak@sprinx.com>
2019-06-06 19:36:46 +0200

    impelemnting calculations

--- src/calculations.js
+++ src/calculations.js
@@ -5,8 +5,7 @@
  * @param  {Number} mglt The Maximum number of Megalights this starship can travel in a standard hour. A "Megalight" is a standard unit of distance and has never been defined before within the Star Wars universe. This figure is only really useful for measuring the difference in speed of starships. We can assume it is similar to AU, the distance between our Sun (Sol) and Earth.
  * @return {Number}     The total amount of stops required to make the distance
  */
-const stopsCalculation = (distance, consumables, mglt) => {
-
-};
+const stopsCalculation = (distance, consumables, mglt) => Math.floor(distance / (mglt * consumables));
+// TODO should be used Math.ceil because if there is some decimal places it means ship is out of consumables. Used Math.floor to keep consistency with a task specification
 
 export default stopsCalculation;
