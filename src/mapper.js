c556ae2ac4f4ebf64ea44df6307e0e7e528d398b
VL K <vladimir.krichtak@sprinx.com>
2019-06-06 14:25:11 +0200

    mapper function signature

---
+++ src/mapper.js
@@ -0,0 +1,12 @@
+/**
+ * Map results
+ * @param  {Number} distance Distance in mega lights (MGLT)
+ * @param  {Object} parsers Property parsers
+ * @param {Object} stopsCalculation Calculation functions
+ * @return {Function}     Mapping function
+ */
+const stopsCalculationMapper = (distance, parsers, stopsCalculation) => (data) => data.map((p) => {
+
+});
+
+export default stopsCalculationMapper;
