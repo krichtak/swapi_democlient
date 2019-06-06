e494d41a10938be3022b6ec18794250d768c8cff
VL K <vladimir.krichtak@sprinx.com>
2019-06-06 20:00:21 +0200

    implementing parsers

--- src/parsers.js
+++ src/parsers.js
@@ -3,8 +3,27 @@
  * @param  {String} mglt The Maximum number of Megalights this starship can travel in a standard hour. A "Megalight" is a standard unit of distance and has never been defined before within the Star Wars universe. This figure is only really useful for measuring the difference in speed of starships. We can assume it is similar to AU, the distance between our Sun (Sol) and Earth.
  * @return {Number}     Result in MGLT
  */
-const parseMGLT = (mglt) => {
+const parseMGLT = (mglt) => parseInt(mglt, 10);
 
+
+const timeConversionTable = {
+  hours: 1,
+  days: 24,
+  weeks: 168,
+  months: 720,
+  years: 8760,
+};
+
+/**
+ * Private function; finds coefficient for conversion in timeConversionTable
+ * @param  {String} unit Time unit
+ * @return {Number}     Amount of days in one unit
+ */
+const findCoefficient = (unit) => {
+  let coefficient = timeConversionTable[unit];
+  if (coefficient) return coefficient;
+  coefficient = timeConversionTable[`${unit}s`];
+  return coefficient;
 };
 
 /**
@@ -13,7 +32,9 @@ const parseMGLT = (mglt) => {
  * @return {Number}     Result in years
  */
 const parseConsumables = (consumables) => {
-
+  const [value, unit] = consumables.split(' ');
+  const coefficient = findCoefficient(unit);
+  return coefficient ? coefficient * value : NaN;
 };
 
 const parsers = { parseMGLT, parseConsumables };
