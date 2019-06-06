3f5d43c474867e62d41711e3ad14c35da432ba22
VL K <vladimir.krichtak@sprinx.com>
2019-06-06 14:30:19 +0200

    renaming calculation function

--- src/calculations.js
+++ src/calculations.js
@@ -5,7 +5,8 @@
  * @param  {Number} mglt The Maximum number of Megalights this starship can travel in a standard hour. A "Megalight" is a standard unit of distance and has never been defined before within the Star Wars universe. This figure is only really useful for measuring the difference in speed of starships. We can assume it is similar to AU, the distance between our Sun (Sol) and Earth.
  * @return {Number}     The total amount of stops required to make the distance
  */
-const calculateStops = (distance, consumables, mglt) => {
+const stopsCalculation = (distance, consumables, mglt) => {
+
 };
 
-export default calculateStops;
+export default stopsCalculation;
--- test/calculationsTest.js
+++ test/calculationsTest.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import calculateStops from '../src/calculations';
+import stopsCalculation from '../src/calculations';
 
 const distance = 1000000;
 
@@ -9,19 +9,19 @@ describe('calculations test', () => {
     it(`on distance ${distance} MGLT should return 
     74 stops for consumables: 168 hours (1 week) and MGLT: 80 (Y-wing)`,
     () => {
-      const result = calculateStops(distance, 168, 80);
+      const result = stopsCalculation(distance, 168, 80);
       expect(result).to.equal(74);
     });
     it(`on distance ${distance} MGLT should return 
     9 stops for consumables: 1440 hours (2 months) and MGLT: 75 (Millennium Falcon)`,
     () => {
-      const result = calculateStops(distance, 1440, 75);
+      const result = stopsCalculation(distance, 1440, 75);
       expect(result).to.equal(9);
     });
     it(`on distance ${distance} MGLT should return 
     11 stops for consumables: 4320 hours (6 months) and MGLT: 20 (Rebel Transport)`,
     () => {
-      const result = calculateStops(distance, 4320, 20);
+      const result = stopsCalculation(distance, 4320, 20);
       expect(result).to.equal(11);
     });
   });
